e2894cbc77f19905c5fe6fbc354479e05ff5a81d
dragoslavIvkovic <ivkovic.dragoslav.dragi@gmail.com>
2021-01-16 09:19:32 +0000

    img

--- src/components/pages/Desserts.js
+++ src/components/pages/Desserts.js
@@ -8,33 +8,33 @@ export default function Desserts() {
     <div className="flex-container">
       <div className="Card">
         <PizzasProps
-          img="https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          img="https://images.unsplash.com/photo-1607206608117-31f7a8a0ee46?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=927&q=80"
+          name="Cake" />
         <Button />
 
       </div>
       <div className="Card">
         <PizzasProps
-          img="https://images.unsplash.com/photo-1597895139270-a5dee112224d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          img="https://images.unsplash.com/photo-1531240062960-4842b265a1ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=853&q=80"
+          name="Ice Cream" />
         <Button />
       </div>
       <div className="Card">
         <PizzasProps
-          img="https://images.unsplash.com/photo-1575389371771-2ee5b9bba753?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          img="https://images.unsplash.com/photo-1588258382591-5725de4bb861?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          name="Chocolate" />
         <Button />
       </div>
       <div className="Card">
         <PizzasProps
-          img="https://images.unsplash.com/photo-1571506636496-dd2c945bde72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          img="https://images.unsplash.com/photo-1528207776546-365bb710ee93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          name="Pancake" />
         <Button />
       </div>
       <div className="Card">
         <PizzasProps
-          img="https://images.unsplash.com/photo-1515544699701-86e5f26b66d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          img="https://images.unsplash.com/photo-1576618148400-f54bed99fcfd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+          name="Cupcake" />
         <Button />
       </div>
 
--- src/components/pages/Pizzas.js
+++ src/components/pages/Pizzas.js
@@ -9,20 +9,20 @@ export default function Pizzas() {
       <div className="Card">
         <PizzasProps
           img="https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          name="Capricciosa" />
         <Button />
 
       </div>
       <div className="Card">
         <PizzasProps
           img="https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          name="Bob" />
+          name="Margherita " />
         <Button />
       </div>
       <div className="Card">
         <PizzasProps
           img="https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=600"
-          name="Bob" />
+          name="Pepperoni " />
         <Button />
       </div>
 
